d5f28778d519d703d573806dd66beeb7436fdf2a
cord123123 <52894859+cord123123@users.noreply.github.com>
2021-02-20 18:43:55 +0100

    fixed formatting

--- src/main/java/com/example/restservice/Controler.java
+++ src/main/java/com/example/restservice/Controler.java
@@ -11,43 +11,43 @@ public class Controler {
 
     @GetMapping("/calculate")
     public String response(@RequestParam(value = "number") String inputNumber,
-                           @RequestParam(value = "system") String numericSystem){
+                           @RequestParam(value = "system") String numericSystem) {
         String calculatedNumber;
         int intNumber;
-        try{
-         intNumber= Integer.parseInt(inputNumber);
-        }catch (NumberFormatException e){
+        try {
+            intNumber = Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
             return "Only integers allowed.";
         }
 
-        if(numericSystem.toLowerCase().equals("hex")){
-            calculatedNumber=Integer.toHexString(intNumber);
-        }else if(numericSystem.toLowerCase().equals("roman")){
-            try{
-                calculatedNumber=intToRoman(intNumber);
-            }catch(IntegerToRomanException e){
+        if (numericSystem.toLowerCase().equals("hex")) {
+            calculatedNumber = Integer.toHexString(intNumber);
+        } else if (numericSystem.toLowerCase().equals("roman")) {
+            try {
+                calculatedNumber = intToRoman(intNumber);
+            } catch (IntegerToRomanException e) {
                 return e.getMessage();
             }
 
-        }else{
+        } else {
             return "System not implemented";
         }
 
-        return String.format(template,inputNumber,numericSystem,calculatedNumber);
+        return String.format(template, inputNumber, numericSystem, calculatedNumber);
     }
 
     private String intToRoman(int input) throws IntegerToRomanException {
         StringBuilder stringBuilder = new StringBuilder();
-        String[] romanSigns = {"I","IV","V","IX","X","XL","L","XC","C","LD","D","LM","M"};
-        int[] numbers = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
-        int index=numbers.length-1;
-        if(input<1||input>3999) throw new IntegerToRomanException(input);
+        String[] romanSigns = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "LD", "D", "LM", "M"};
+        int[] numbers = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        int index = numbers.length - 1;
+        if (input < 1 || input > 3999) throw new IntegerToRomanException(input);
 
-        while(input>0){
-            if(input>=numbers[index]){
-                input-=numbers[index];
+        while (input > 0) {
+            if (input >= numbers[index]) {
+                input -= numbers[index];
                 stringBuilder.append(romanSigns[index]);
-            }else
+            } else
                 index--;
         }
         return stringBuilder.toString();
