4c6f6f3c21dc1107d0d293a2d9bd78317a3aa9b0
cord123123 <52894859+cord123123@users.noreply.github.com>
2021-02-24 20:55:14 +0100

    Added documentation

--- src/main/java/com/example/restservice/NumberConverterController.java
+++ src/main/java/com/example/restservice/NumberConverterController.java
@@ -1,11 +1,14 @@
 package com.example.restservice;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class NumberConverterController {
@@ -17,16 +20,15 @@ public class NumberConverterController {
         this.service = service;
     }
 
+    @Operation(summary = "Convert integer number different numeric system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Calculated."),
+            @ApiResponse(responseCode = "400", description = "Invalid data supplied.")
+    })
     @GetMapping("/number_converter")
-    public String convertNumber(@RequestParam int number,
-                                @RequestParam String system) {
-        try {
-            return service.getConvertedNumber(system, number);
-        } catch (NumericSystemNotSupported numericSystemNotSupported) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Numeric system not supported.",numericSystemNotSupported);
-        } catch (RomanNumberOutOfRangeException romanNumberOutOfRangeException) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Conversion in range 1-3999 only supported.",romanNumberOutOfRangeException);
-        }
+    public String convertNumber(@Parameter(description = "Int number to convert.") @RequestParam int number,
+                                @Parameter(description = "Numeric system {roman,hex}.") @RequestParam String system) {
+        return service.getConvertedNumber(system, number);
     }
 }
 
